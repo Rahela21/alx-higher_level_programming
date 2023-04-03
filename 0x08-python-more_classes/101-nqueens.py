1a9534d70bd777311d10f893b14a746847b4d6b8
Rahela21 <rahelberhe216@gmailcom>
2023-04-03 11:57:07 -0700

    python more classes and objects

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -52,7 +52,7 @@ def xout(board, row, col):
 
     All spots where non-attacking queens can no
     longer be played are X-ed out.
-
+    
     Args:
         board (list): The current working chessboard.
         row (int): The row where a queen was last played.
@@ -102,7 +102,7 @@ def xout(board, row, col):
 
 def recursive_solve(board, row, queens, solutions):
     """Recursively solve an N-queens puzzle.
-
+    
     Args:
         board (list): The current working chessboard.
         row (int): The current working row.
